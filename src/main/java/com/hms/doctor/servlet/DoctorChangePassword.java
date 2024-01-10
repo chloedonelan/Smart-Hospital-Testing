8878e12a716f37627a37f41def959b8316bba936
nandan.s <nandan.s@LP-5CD141BBB1.HCLT.CORP.HCL.IN>
2024-01-10 12:32:20 +0530

    initial commit

---
+++ src/main/java/com/hms/doctor/servlet/DoctorChangePassword.java
@@ -0,0 +1,55 @@
+package com.hms.doctor.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import com.hms.dao.DoctorDAO;
+import com.hms.db.DBConnection;
+
+@WebServlet("/doctorChangePassword")
+public class DoctorChangePassword extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		int doctorId = Integer.parseInt(req.getParameter("doctorId"));
+		String newPassword = req.getParameter("newPassword");
+		String oldPassword = req.getParameter("oldPassword");
+
+		DoctorDAO doctorDAO = new DoctorDAO(DBConnection.getConn());
+
+		HttpSession session = req.getSession();
+
+		if (doctorDAO.checkOldPassword(doctorId, oldPassword)) {
+
+			if (doctorDAO.changePassword(doctorId, newPassword)) {
+				
+				session.setAttribute("successMsg", "Password change successfully.");
+				resp.sendRedirect("doctor/edit_profile.jsp");
+
+			} else {
+				
+				session.setAttribute("errorMsg", "Something went wrong on server!");
+				resp.sendRedirect("doctor/edit_profile.jsp");
+
+			}
+
+		} else {
+			session.setAttribute("errorMsg", "Old Password not match");
+			resp.sendRedirect("doctor/edit_profile.jsp");
+
+		}
+	}
+
+}
