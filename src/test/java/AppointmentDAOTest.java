9967fcf7d66b42a36e5a9abdbfbfee31a88cfef4
Chloe Donelan <chloemdonelan@gmail.com>
2025-04-18 14:01:16 -0400

    set up nested classes for testing

---
+++ src/test/java/AppointmentDAOTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Nested;
+
+public class AppointmentDAOTest {
+  @Nested
+  public class ConstructorTests {
+    // TODO
+  }
+  
+  @Nested
+  public class addAppointmentTests {
+    // TODO
+  }
+  
+  @Nested
+  public class getAllAppointmentByLoginUserTests {
+    // TODO
+  }
+  
+  @Nested
+  public class getAllAppointmentByLoginDoctorTests {
+    // TODO
+  }
+  
+  @Nested
+  public class getAppointmentByIdTests {
+    // TODO
+  }
+  
+  @Nested
+  public class updateDrAppointmentCommentStatusTests {
+    // TODO
+  }
+  
+  @Nested
+  public class getAllAppointmentTests {
+    // TODO
+  }
+}
