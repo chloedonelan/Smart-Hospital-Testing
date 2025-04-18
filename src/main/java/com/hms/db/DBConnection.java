f7268cbf5d3185293969c2df1cd9b9762df0879e
Jonathan Liu <65567794+32111jl@users.noreply.github.com>
2025-04-18 15:48:08 -0400

    renaming hospital to hospital_db in DBConnection so selenium tests work

--- src/main/java/com/hms/db/DBConnection.java
+++ src/main/java/com/hms/db/DBConnection.java
@@ -15,7 +15,7 @@ public class DBConnection {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
 			//step:2- create a connection
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital","root","Nandan185$");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital_db","root","Nandan185$");
 			
 		} catch (Exception e) {
 			e.printStackTrace();
