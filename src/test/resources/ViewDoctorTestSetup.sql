33fd6e4ac000c39426b6ad3f3b57c01031e8309a
Chloe Donelan <chloemdonelan@gmail.com>
2025-04-23 01:31:50 -0400

    updates

--- src/test/resources/ViewDoctorTestSetup.sql
+++ src/test/resources/ViewDoctorTestSetup.sql
@@ -5,7 +5,7 @@ TRUNCATE TABLE doctor;
 SET FOREIGN_KEY_CHECKS=1;
 
 INSERT INTO doctor (fullName, dateOfBirth, qualification, specialist, email, phone, password)
-VALUES ('Sally Smith', '1978-01-06', 'MD/PhD', 'Cardiology', 'ssmith@gmail.com', '567-555-4673', 'Sally!');
+VALUES ('Sally Smith', '1978-01-06', 'MD/PhD', 'Cardiology', 'ssmith@gmail.com', '567-555-4673', 'S@lly!');
 
 INSERT INTO doctor (fullName, dateOfBirth, qualification, specialist, email, phone, password)
-VALUES ('Steve Winthrop', '1995-05-17', 'MD', 'Plastic Surgery', 'stevew@gmail.com', '322-555-6123', 'Steve');
+VALUES ('Steve Winthrop', '1995-05-17', 'MD', 'Plastic Surgery', 'stevew@gmail.com', '322-555-6123', 'St3v3@');
