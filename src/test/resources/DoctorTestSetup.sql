abd8bb336262d35873b22e197f3ef3f9e3b44c9e
Chloe Donelan <chloemdonelan@gmail.com>
2025-04-26 14:57:27 -0400

    added for testing purposes

---
+++ src/test/resources/DoctorTestSetup.sql
@@ -0,0 +1,11 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+TRUNCATE TABLE specialist;
+
+SET FOREIGN_KEY_CHECKS=1;
+
+INSERT INTO specialist (specialist_name)
+VALUES ('Neurology');
+
+INSERT INTO specialist (specialist_name)
+VALUES ('Cardiology');
